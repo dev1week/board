eadc349c7ee9f1f91925542bae7e2b4cc333c62b
dev1week <rondo2860@gmail.com>
2023-02-15 23:50:16 +0900

    # 10 - jpa test 작성
    - crud 메서드를 포함하는 테스트 작성 
    - 현재 data.sql(초기 데이터)과 분리한 채로 커밋함

---
+++ board/src/test/java/com/example/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.example.board.repository;
+
+import com.example.board.config.JpaConfig;
+import com.example.board.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+    @Autowired
+    private ArticleRepository articleRepository;
+    @Autowired
+    private ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository)
+    {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine(){
+        Article testArticle = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        long deletedCommentsSize = testArticle.getArticleComments().size();
+
+        articleRepository.delete(testArticle);
+
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount-1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount-deletedCommentsSize);
+    }
+}
