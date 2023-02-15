e2fa64583270aa2f34bf821c4897135a020458f0
dev1week <rondo2860@gmail.com>
2023-02-15 23:48:28 +0900

    #9 - 도메인을 엔티티로 변환함

--- board/src/main/java/com/example/board/domain/Article.java
+++ board/src/main/java/com/example/board/domain/Article.java
@@ -1,19 +1,95 @@
 package com.example.board.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes ={
+    @Index(columnList = "title"),
+    @Index(columnList = "hashtag"),
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
+    @Column(nullable = false)
     private String title;
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String Content;
+    @Setter
     private String hashtag;
 
+    @OrderBy("id")
+    @OneToMany(mappedBy="article",cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+
+
+    protected Article() {
+    }
+
+    //메타 데이터와 id(자동으로 들어가는 값)을 제외한 멤버변수를 생성하는 생성자
+    //실제 사용시에 사용을 간편하게 하기 위함
+    //이후 of 메서드로 생성하게끔 유도함
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        Content = content;
+        this.hashtag = hashtag;
+    }
+
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
